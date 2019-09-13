b709eb46a37f7fa290e31b29cb41d686e6cdbc20
Bas_van_Asten <16081242@student.hhs.nl>
2019-09-13 15:01:39 +0200

    Made a new assignment .py and edited the old one to contain subplots.

--- Digitale signaalverwerking/Code/PythonOpdracht1.py
+++ Digitale signaalverwerking/Code/PythonOpdracht1.py
@@ -10,34 +10,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #opdracht A constanten
-num = [1,1]
-den = [1,0]
+a = 1/2
+num = [1, 1]
+den = [1, 0]
 len_ = 10
 dt = 1
-omega = np.arange(-np.pi,np.pi,0.05)
 
 #plotten van opdracht A
 plt.figure(0)
-t,y = scig.dimpulse((num,den,dt),n=len_)
-plt.stem(t,y[0])
+plt.subplot(311)
+n,y = scig.dimpulse((num,den,dt),n=len_)
+plt.stem(n,y[0])
 plt.grid()
 plt.xlabel('n' )
-plt.ylabel('h(n)')
+plt.ylabel('h[n]')
+plt.title('Impulse Response')
 
 #opdracht B constanten
 omega = np.arange(-np.pi,np.pi,0.05)
 
 #plotten van opdracht B
-plt.figure(1)
+plt.subplot(312)
 w,H = scig.freqz(num,a=den,worN=omega,whole=False)
 plt.plot(w/np.pi,np.abs(H))
 plt.grid()
 plt.xlabel('Omega in pi rad' )
 plt.ylabel('|H(omega)|')
+plt.title('Absolute Transfer')
+
 
 #plotten van opdracht C
-plt.figure(2)
-plt.plot(w,np.angle(H))
+plt.subplot(313)
+plt.plot(w/np.pi,np.angle(H))
 plt.grid()
 plt.xlabel('Omega in pi rad' )
 plt.ylabel('Fase H(omega)')
+plt.title('Transfer Fase')
---
+++ Digitale signaalverwerking/Code/PythonOpdracht2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 14:39:57 2019
+
+@author: beli
+"""
+
