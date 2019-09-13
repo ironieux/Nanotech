93b187add8fcb0f46438150c4972e5dea6f2b407
Bas_van_Asten <16081242@student.hhs.nl>
2019-09-13 10:57:22 +0200

    Added some comments in the first assignment for signal processing

--- Digitale signaalverwerking/Code/PythonOpdracht1.py
+++ Digitale signaalverwerking/Code/PythonOpdracht1.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Opdracht 1 van Digitale Signaalbewerking
+    a: stepresponse
+    b: absoverdracht over frequentie
+    c: faseoverdracht over frequentie
 """
 import scipy.signal as scig
 import matplotlib.pyplot as plt
 import numpy as np
 
+#opdracht A constanten
 num = [1,1]
 den = [1,0]
 len_ = 10
+dt = 1
 omega = np.arange(-np.pi,np.pi,0.05)
 
+#plotten van opdracht A
 plt.figure(0)
-t,y = scig.dimpulse((num,den,0.1),n=len_)
+t,y = scig.dimpulse((num,den,dt),n=len_)
 plt.stem(t,y[0])
 plt.grid()
 plt.xlabel('n' )
 plt.ylabel('h(n)')
 
+#opdracht B constanten
+omega = np.arange(-np.pi,np.pi,0.05)
+
+#plotten van opdracht B
 plt.figure(1)
 w,H = scig.freqz(num,a=den,worN=omega,whole=False)
 plt.plot(w/np.pi,np.abs(H))
@@ -27,6 +35,7 @@ plt.grid()
 plt.xlabel('Omega in pi rad' )
 plt.ylabel('|H(omega)|')
 
+#plotten van opdracht C
 plt.figure(2)
 plt.plot(w,np.angle(H))
 plt.grid()
